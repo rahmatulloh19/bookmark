94c92b335c4cab6e83568038d7f83bb776de0fb3
rahmatulloh19 <raxmonovrahmatulloh@gmail.com>
2023-12-12 03:26:15 +0500

    complate hero section

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,26 @@
 module.exports = {
 	content: ["./*.{html,js}"],
 	theme: {
-		extend: {},
+		extend: {
+			screens: {
+				mm: "375px",
+				md: "768px",
+				lg: "1024px",
+				xl: "1148px",
+			},
+			container: {
+				center: true,
+				padding: "32px",
+				screens: {
+					DEFAULT: "1148px",
+				},
+			},
+			colors: {
+				mainColor: "#242a45",
+				mainBgColor: "#5267df",
+				buttonBgColor: "#fa5959",
+			},
+		},
 	},
 	plugins: [],
 };
